a2837b1ca9f8fed058ccc720d060447bab5f1977
ViniIsOn <pontodecena@gmail.com>
2026-09-05 10:06:51 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'orbclash-v81-pwa-1';
+const CORE = ['./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const req = event.request;
+  if (req.mode === 'navigate') {
+    event.respondWith(fetch(req).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(cache => cache.put('./index.html', copy));
+      return resp;
+    }).catch(() => caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(req).then(hit => hit || fetch(req).then(resp => {
+    const copy = resp.clone();
+    caches.open(CACHE).then(cache => cache.put(req, copy));
+    return resp;
+  })));
+});
